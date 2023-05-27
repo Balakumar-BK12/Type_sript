3fa0dfcb0bae79f63362f58227ef72801da65e44
Balakumar-BK12 <kumarbala5337@gmail.com>
2023-05-27 21:20:29 +0530

    adv_class_ts/Functions/Rest_Parameters.ts

---
+++ adv_class_ts/Functions/Rest_Parameters.ts
@@ -0,0 +1,21 @@
+// Rest Parameters
+
+/*
+
+A rest parameter allows you a function to accept zero or more arguments of the specified type. 
+In TypeScript, rest parameters follow these rules:
+
+A function has only one rest parameter.
+The rest parameter appears last in the parameter list.
+The type of the rest parameter is an array type.
+
+*/
+
+function getTotal(...numbers: number[]): number {
+    let total = 0;
+    numbers.forEach((num) => total += num);
+    return total;
+}
+console.log(getTotal()); // 0
+console.log(getTotal(10, 20)); // 30
+console.log(getTotal(10, 20, 30)); // 60
